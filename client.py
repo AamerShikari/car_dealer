dff69fd1f9c8af717751b7ec271e09e17ec24a29
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2021-05-09 12:58:30 -0400

    add user verficiation

--- client.py
+++ client.py
@@ -10,15 +10,19 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
-my_username = input("Username: ") 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
 client_socket.setblocking(False) #sets receive blocking to false -> can receive 
 
-
+my_username = input("Username: ") 
 username = my_username.encode("utf-8")
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
 client_socket.send(username_header + username)
+
+my_password = input("Password: ") 
+password = my_password.encode("utf-8")
+password_header = f"{len(password):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(password_header + password)
 
 time.sleep(1)
 
@@ -31,9 +35,12 @@ while True:
     message = input(f"{my_username} > ")
 
     if message: 
+        if message == "PURCHASE":
+            print(f"Server > Please enter your password:")
         message = message.encode("utf-8")
         message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
         client_socket.send(message_header + message)
+        
     try:
         while True: 
             #receive things 
--- server.py
+++ server.py
@@ -8,31 +8,39 @@ class Blockchain:
     def __init__(self):
         self.chain = []  # blockchain
         self.transaction = []  # transaction to add to block
+        self.users = [] #list of users
         #self.request = []
         self.car = []
         #self.money = []
-        self.new_block("Tony", "Ferrari", '0')
+        self.new_block("Tony", "tony123", "Ferrari", '0')
+        self.new_user("Tony", "tony123")
 
-    def new_block(self, username, car_value, previous_hash):
+    def new_block(self, username, password, car_value, previous_hash):
         block = {
             #'index': len(self.chain) + 1,
             "user": username,
+            "pwrd": password,
             'car': car_value,
             'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
         self.transaction.append([username, car_value]) #add money or additional features 
+
         #self.request = []
         #self.car = []
         self.chain.append(block)
         return block
 
+    def new_user(self, username, password):
+        self.users.append([username, password])
+
     def add_car(self, car_value):
         self.car.append(car_value)
 
-        # validate chain by checking previous hashes
+    # validate chain by checking previous hashes
     def validate(self):
         previous_block = self.chain[0]
         counter = 1
+
         while counter < len(self.chain):
             current_block = self.chain[counter]
             previous_hash = self.hash(previous_block)
@@ -42,12 +50,63 @@ class Blockchain:
             counter += 1
         return True
 
+    def check_user(self, user, pword):
+        username = user['data'].decode('utf-8')
+        password = pword['data'].decode('utf-8')
+
+        found = False
+
+        for x in self.users:
+            if x[0] == username:
+                found = True
+                if x[1] == password:
+                    return True
+                else:
+                    print(f"Incorrect password.")
+                    return False
+
+        if found == False:
+            self.users.append([username, password])
+            return True
+
+    def valid_buyer(self, user):
+        username = user['data'].decode('utf-8')
+        print(f"Current user: {username}")
+
+        password = receive_message(notified_socket)
+
+        print(f"{self.chain[len(self.chain)-1]['user']}")
+        if self.chain[len(self.chain)-1]['user'] == username:
+            print(f"You already own the vehicle!")
+            return False
+
+        for x in self.chain:
+            if x['user'] == username:
+                pwrd = password['data'].decode('utf-8')
+                if x['pwrd'] != pwrd:
+                    print(f"Incorrect password.")
+                    print(f"Purchase invalid.")
+                    return False
+        
+        if self.check_user(user, password) is False:
+            print(f"Purchase invalid.")
+            return False
+
+
+        print(f"Purchase valid. You now own the vehicle.")
+        return True
+
     def new_transaction(self, owner, car):
         self.transaction.append({
             'owner': owner,
             'car': car
         })
         return self.last_block['index'] + 1
+
+    def remove_transaction(self):
+        self.transaction.pop()
+        self.chain.pop()
+
 
 	
     @staticmethod
@@ -61,6 +120,10 @@ class Blockchain:
 
     def print_blockchain(self):
         for x in self.chain: 
+            print (f"{x}")
+
+    def print_users(self):
+        for x in self.users:
             print (f"{x}")
 
     def print_last(self):
@@ -112,12 +175,15 @@ while True:
                 continue
             socket_list.append(client_socket)
 
+            password = receive_message(client_socket)
             clients[client_socket] = user
-            message = f"Connection confirmed with user: {user['data'].decode('utf-8')}"
-            message = message.encode("utf-8")
-            message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
-            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
-            client_socket.send(message_header + message)
+
+            if block.check_user(user, password) is True:
+                message = f"Connection confirmed with user: {user['data'].decode('utf-8')}"
+                message = message.encode("utf-8")
+                message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+                print (f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}, password: {password['data'].decode('utf-8')}")
+                client_socket.send(message_header + message)
 
         else: 
             message = receive_message(notified_socket)
@@ -134,12 +200,18 @@ while True:
 
             if message['data'].decode('utf-8') == "PURCHASE":
                 #if (block.last_block().decode('utf-8') != user['data'].decode('utf-8')):
-                block.new_block(f"{user['data'].decode('utf-8')}", "Ferarri", '0')
-                print(f"You already own the Vehicle!")
+                if block.valid_buyer(user) == True:
+                    block.new_block(f"{user['data'].decode('utf-8')}", f"{password['data'].decode('utf-8')}", "Ferarri", '0')
+                
+                block.validate()
+                #print(f"You already own the Vehicle!")
                 block.print_last()
 
             if message['data'].decode('utf-8') == "VIEW":
                 block.print_blockchain() 
+
+            if message['data'].decode('utf-8') == "USERS":
+                block.print_users() 
             
 
             #Currently shows all clients messages that one has sent 
