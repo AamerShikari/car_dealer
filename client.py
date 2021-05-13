eb22f463b6c147dce882f73bafaba560def07307
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2021-05-13 16:07:24 -0400

    update user verification

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ PORT = 1234
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-client_socket.setblocking(False) #sets receive blocking to false -> can receive 
+#client_socket.setblocking(False) #sets receive blocking to false -> can receive 
 
 message = ""
 
@@ -34,45 +34,148 @@ while message == "":
     message = client_socket.recv(message_length).decode("utf-8")
     if message == "":
         print(f"Server > Incorrect password")
+
 print(f"Server > {message}")
 
-while True: 
+status = ""
+message_header = client_socket.recv(HEADER_LENGTH)
+message_length = int(message_header.decode("utf-8").strip())
+message = client_socket.recv(message_length).decode("utf-8")
+print(f"Server > Welcome {message}")
+status = message
+
+while True:
+
     message = input(f"{my_username} > ")
 
-    if message: 
-        if message == "PURCHASE":
-            print(f"Server > Please enter your password:")
-        if message == "Q":
-            print(f"Quit")
-            sys.exit()
+    if message == "PURCHASE" and status == "BUYER":
+        #sending purchase request
         message = message.encode("utf-8")
         message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
         client_socket.send(message_header + message)
-        
-    try:
-        while True: 
-            #receive things 
-            #Setup the username length and data
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print("connection closed by the server")
-                sys.exit()
-            username_length = int(username_header.decode("utf-8").strip())
-            username = client_socket.recv(username_length).decode("utf-8")
 
-            #Setup the message length and header 
+        #receive request confirmation
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+        print(f"Server > {message}")
+
+        #receive SELLER response
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+        print(f"Server > {message}")
+
+        if message != "Request denied.":
+            #input password to pay
+            message = input(f"{my_username} > ")
+            message = message.encode("utf-8")
+            message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+            client_socket.send(message_header + message)
+
+            #receive purchase confirmation or error
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode("utf-8").strip())
             message = client_socket.recv(message_length).decode("utf-8")
+            print(f"Server > {message}")
 
-            print(f"{username} > {message}")
-    
-    except IOError as e: 
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error', str(e))
-            sys.exit()
-        continue 
+            if message != "Password incorrect. Purchase invalid.":
+                status = "SELLER"
+                print(f"You are now a seller.")
 
-    except Exception as e: 
-        print('General error', str(e))
+    elif message == "PURCHASE" and status == "SELLER":
+        print(f"Server > You already own the car.")
+    elif message == "SELL" and status == "SELLER":
+        print(f"Waiting for a buyer...")
+
+        #receive BUYER request
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+        print(f"Server > {message}")
+
+        #send decision 
+        message = input(f"{my_username} > ")
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+        #receive decision confirmation
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+        print(f"Server > {message}")
+
+        #receive purchase confirmation or error
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+        print(f"Server > {message}")
+
+        if message != "Buyer invalid. Purchase incomplete.":
+            status = "BUYER"
+            print(f"You are now a buyer.")
+    elif message == "SELL" and status == "BUYER":
+        print(f"You have no cars to sell.")
+    elif message == "ADD":
+        #sending deposit request
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+        #enter amount to deposit
+        print(f"Server > Enter amount:")
+        message = input(f"{my_username} > ")
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+    elif message == "Q":
+        print(f"Quit")
         sys.exit()
+    else:
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+        
+        
+
+# while True: 
+#     message = input(f"{my_username} > ")
+
+#     if message: 
+#         if message == "PURCHASE":
+#             print(f"Server > Please enter your password:")
+#         if message == "Q":
+#            
+#         message = message.encode("utf-8")
+#         message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+#         client_socket.send(message_header + message)
+        
+#     try:
+#         while True: 
+#             #receive things 
+#             #Setup the username length and data
+#             username_header = client_socket.recv(HEADER_LENGTH)
+#             if not len(username_header):
+#                 print("connection closed by the server")
+#                 sys.exit()
+#             username_length = int(username_header.decode("utf-8").strip())
+#             username = client_socket.recv(username_length).decode("utf-8")
+
+#             #Setup the message length and header 
+#             message_header = client_socket.recv(HEADER_LENGTH)
+#             message_length = int(message_header.decode("utf-8").strip())
+#             message = client_socket.recv(message_length).decode("utf-8")
+
+#             print(f"{username} > {message}")
+    
+#     except IOError as e: 
+#         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+#             print('Reading error', str(e))
+#             sys.exit()
+#         continue 
+
+#     except Exception as e: 
+#         print('General error', str(e))
+#         sys.exit()
